3d3be06488fce502ccaba6b98255032da353506e
Vinicios Toledo <viabolafio@hotmail.com>
2023-06-21 15:34:34 -0300

    Criação do middleware para validação de dados do POST

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const tasksController = require(`./controllers/taskController`)
+const tasksMiddlewares = require(`./middlewares/tasksMiddleware`)
 
 const router = express.Router()
 
 router.get('/tasks', tasksController.getAll)
-router.post('/tasks', tasksController.createTask)
+router.post('/tasks', tasksMiddlewares.validateBody ,tasksController.createTask)
 module.exports = router
